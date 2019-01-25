7096e80f67b479f8aff650034ccec0b1fe3f04e1
thomasJur <pyristfeuer@googlemail.com>
2019-01-25 16:09:09 +0100

    now the password needs to be at least 4 char long and has to have a string, a digit, and a punctuation

--- create.py
+++ create.py
@@ -3,15 +3,33 @@ import sys
 import string
 
 length=0
+
 if len(sys.argv) > 1:
-	length = int(sys.argv[1])
+	if int(sys.arg[1]) > 3:
+		length = int(sys.argv[1])
+	else:
+		print("Your password needs to be at least 4 characters long!")
 else:
 	length = 12
-
 password = []
+digit = False
+string_char = False
+punctuation = False
 
-for i in range(length):
-	password.append(random.choice(string.ascii_letters + string.digits))
+while (not(digit and string_char and punctuation)):
+	password = []
+	digit = False
+	string_char = False
+	punctuation = False
+	for i in range(length):
+		character = random.choice(string.ascii_letters + string.digits + "!?-;:")
+		password.append(character)
+		if character in string.ascii_letters:
+			string_char = True
+		elif character in string.digits:
+			digit = True
+		elif character in ["!", "?", "-", ";", ":"]:
+			punctuation = True
 
 password = "".join(password)
 
